1116eda2dcdb2d4925b2a702c473d73de83b4a59
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-12 17:09:11 -0400

    Send name string to server

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+const net = require("net");
+
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
@@ -8,13 +10,21 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
   
-  // conn.on("connect", () => {
-  //   console.log("Connected to Snek!");
-  // });
+  conn.on("connect", () => {
+    console.log("You sneaky Snek!  You're connected ...");
+  });
 
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
+
+  conn.on("connect", () => {
+    conn.write("Name: Vik");
+  });
+  
+
   
   return conn;
 };
+
+module.exports = {connect};
