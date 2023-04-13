c600b89344364ae1073e70c2f3c4ef1ef25e0f97
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-13 00:19:54 -0400

    Add movement functionality to snake

--- input.js
+++ input.js
@@ -1,6 +1,10 @@
 const net = require("net");
 
-const setupInput = function () {
+
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -15,8 +19,25 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
-};
 
-setupInput();
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+
+  if (key === 'd') {
+    connection.write("Move: right");
+ }
+}
+
+// setupInput(connect());
+
 
 module.exports = {setupInput}
--- play.js
+++ play.js
@@ -3,5 +3,6 @@ const {connect} = require("./client");
 const {setupInput} = require("./input");
 
 console.log("Connecting ...");
-connect();
+//connect();
 
+setupInput(connect());
